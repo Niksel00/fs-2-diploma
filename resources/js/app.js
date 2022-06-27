90b3d8fc0575a4f419d15b55cc4bfcaf2267127c
Niksel00 <nikita-efremov-00@mail.ru>
2022-06-27 17:52:56 +0400

    Diploma v.1.0

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Application from './Application';
+
+require('./bootstrap');
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Application />
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
